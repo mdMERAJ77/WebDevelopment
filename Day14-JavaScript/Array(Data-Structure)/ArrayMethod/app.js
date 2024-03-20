b8875a5760e95821b8caaefc9cfe2932057a2276
MD MERAJ <mmd200975@gmail.com>
2024-03-20 20:24:31 +0530

    Successfully Commit

---
+++ Day14-JavaScript/Array(Data-Structure)/ArrayMethod/app.js
@@ -0,0 +1,9 @@
+let Cars=['audi','bmw','xuv','maruti'];
+Cars.push('toyota');
+console.log(Cars);
+Cars.pop();
+console.log(Cars);
+Cars.unshift('toyota');
+console.log(Cars);
+Cars.shift();
+console.log(Cars);
---
+++ Day14-JavaScript/Array(Data-Structure)/app.js
@@ -0,0 +1,12 @@
+let students = ['meraj','sakib','sarik','hamza'];
+
+let marks=[99,89,78,45,58,55,48,12]; 
+let names=['meraj','sakib','sarik','hamza'];
+let info=["meraj",25,6.1]; // mixed array
+let newArr=[]; //empty array
+
+// Arrays are Mutable
+let fruits = ['mango','apple','litchi'];
+fruits[0]='banana';
+fruits[10]='papaya';
+console.log(fruits);
