412acda0c8b0fb8f5ef347ac874628d6ab15f089
MD MERAJ <mmd200975@gmail.com>
2024-03-20 14:22:42 +0530

    Successfully Commit

---
+++ Day13-JavaScript/String-Method/PraticeQ/app.js
@@ -0,0 +1,8 @@
+let msg="    help!   ";
+console.log(msg.trim().toUpperCase()); //HELLP!
+
+let str="ApnaCollege";
+console.log(str.slice(4,9)); //Colle
+console.log(str.indexOf('na')); //2
+console.log(str.replace('Apna','Our'));
+console.log(str.slice(4).replace('l','t').replace('l','t')); //Cottege
---
+++ Day13-JavaScript/String-Method/Replace&Repeat-Method/app.js
@@ -0,0 +1,3 @@
+let str = "ILoveCoding";
+console.log(str.replace("Love","do"));
+console.log(str.repeat(3))
