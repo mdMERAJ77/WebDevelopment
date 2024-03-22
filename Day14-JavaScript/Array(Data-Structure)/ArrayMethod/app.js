602428a4621a9e6d8d3ae4455595bc447f240d80
MD MERAJ <mmd200975@gmail.com>
2024-03-23 00:34:40 +0530

    Successfully Commit

--- Day14-JavaScript/Array(Data-Structure)/ArrayMethod/app.js
+++ Day14-JavaScript/Array(Data-Structure)/ArrayMethod/app.js
@@ -16,7 +16,7 @@ arr.shift();
 arr.unshift('june');
 console.log(arr);
 
-// indexOf:returns index of something  and includes:search for a value
+// indexOf method:returns index of something  and includes:search for a value
 let color = ['red','yellow','blue'];
 
 console.log(color.indexOf('red')); //0
@@ -24,12 +24,31 @@ console.log(color.includes('red')); //true
 console.log(color.indexOf('blue')); //2
 console.log(color.includes('green')); //false
 
-// concat:merge 2 Arrays
+// concat method:merge 2 Arrays
 let first =['a','b','c','d'];
 let second =['e','f','g','h'];
 console.log(first.concat(second));
 console.log(second.concat(first));
 
-// reverse:reverse an array
+// reverse method:reverse an array
 let array1=['a','b',1,2];
 console.log(array1.reverse());
+
+//slice method:copies portion of an array
+let color1 =['red','green','yellow','black','cyan'];
+console.log(color1.slice(-1));
+console.log(color1.slice());
+console.log(color1.slice(2,3));
+
+//splice method:change original in array
+let demo=['a','b','c','d','e','f','g'];
+console.log(demo.splice(2,2));
+console.log(demo.splice(1,3));
+console.log(demo.splice(2,0,'x'));
+
+//sort method: sorts an array
+let alphabet =['c','e','g','d','b','i','a','A'];
+console.log(alphabet.sort());
+let number=[5,2,0,5,8,2,6,9,7,3,0,4,5,8];
+
+console.log(number.sort());
