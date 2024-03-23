2da46dfa0531d82b1f3fc73527fb8a25410a0f85
MD MERAJ <mmd200975@gmail.com>
2024-03-23 17:04:55 +0530

    Successfully Commit

---
+++ Day15-JavaScript/For-Loops/app.js
@@ -0,0 +1,21 @@
+ for(let i=1; i<=5; i++){
+    //console.log(i);
+ }
+
+ for(let i=5; i>=1; i--){
+    //console.log(i);
+ }
+
+ // print all odd number (1 to 15)
+ for(let i=1; i<=15; i++){
+    if(i%2!=0){
+        //console.log(i);
+    }
+ }
+
+// print all even numbers (2 to 10)
+for(let i=2; i<=10; i++){
+    if(i%2==0){
+        //console.log(i);
+    }
+}
