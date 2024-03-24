c6a0df445ea00b045c408afed10e3c1abd80c94f
MD MERAJ <mmd200975@gmail.com>
2024-03-25 02:32:59 +0530

    Successfully Commit

---
+++ Day15-JavaScript/for-of-loop/app.js
@@ -0,0 +1,16 @@
+let fruits =['mango','apple','banana','litchi','orange'];
+for(fruit of fruits){
+    //console.log(fruit);
+}
+
+for(char of 'meraj'){
+    //console.log(char);
+}
+
+// Nested for of loop
+let names=[['meraj','sakib'],['sarik'],['hamza']];
+for(list of names){
+    for(name of list){
+        console.log(name);
+    }
+}
