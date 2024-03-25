25a5bc1f810d482234cdddadaccbee885a0d6289
MD MERAJ <mmd200975@gmail.com>
2024-03-25 23:07:35 +0530

    Successfully Commit

--- Day16-JavaScript/CreatingObjectLiterals/app.js
+++
@@ -1,6 +0,0 @@
-const student = {
-    name:'meraj',
-    age:22,
-    marks:94.4,
-    city:'bihar'
-};
---
+++ Day16-JavaScript/ObjectLiterals/app.js
@@ -0,0 +1,12 @@
+const student = {
+    name:'meraj',
+    age:22,
+    marks:94.4,
+    city:'bihar',
+    // get value  student['name'] or student.name;
+};
+
+
+// ADD or UPDATE in object literals:- 
+// ADD:-  student.city='delhi'; or student.hobbies =['basketball','chess'];
+// DELETE:- delete student.marks;
