0eee9189915bee4891d4effb37787bd3c6f8b290
MD MERAJ <mmd200975@gmail.com>
2024-03-30 22:10:26 +0530

    Successfully Commit

---
+++ Day17-JavaScript/Function_With_Argument/app.js
@@ -0,0 +1,12 @@
+function printInfo(name,age){
+    console.log(`Name: ${name}, age is ${age}.`);
+}
+// printInfo('meraj',20);
+// printInfo('sakib');
+
+
+function sum(num1,num2){
+    console.log(num1+num2);
+}
+sum(1,2);
+sum(2,-1);
--- Day17-JavaScript/PraticeQ/app.js
+++ Day17-JavaScript/PraticeQ/app.js
@@ -1,6 +1,27 @@
-function printPoem(){
+function printPoem() {
     console.log('twinkle twinkle little star');
     console.log('how i wonder what you are');
 }
+//printPoem();
 
-printPoem();
+// create a function to roll a dice&always display the value of the dice(1 to 6).
+function rollDice() {
+    let rand = Math.floor(Math.random() * 6) + 1;
+    console.log(rand);
+}
+//rollDice();
+
+// Create a Function that gives us the average of 3 numbers
+function calculateAverage(a,b,c){
+   let avg=(a+b+c)/3;
+    console.log(avg);
+}
+//calculateAverage(2,2,2);
+
+//Create a function that prints the multiplication table of a number
+function printTable(number){
+    for(let i=1;i<=10;i++){
+        console.log(i*number);
+    }
+}
+printTable(5);
