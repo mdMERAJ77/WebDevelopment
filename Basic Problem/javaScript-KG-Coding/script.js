deea3197d9142b16c14301e0512918f6fd545ac5
MD MERAJ <mmd200975@gmail.com>
2024-09-03 06:53:17 +0530

    Successfully Commit

---
+++ Basic Problem/javaScript-KG-Coding/script.js
@@ -0,0 +1,17 @@
+
+console.log('hello');
+console.warn('warning');
+console.error('stop');
+// console.clear()
+
+// document.querySelector("#btn").style.background = 'red';
+
+// Update the document title instead of innerText
+document.title = 'javaScript';
+
+var bagButton = document.querySelector('.bag-button');
+
+
+
+var wishButton = document.querySelector('.wish-button');
+// Perform actions on wishButton if needed
